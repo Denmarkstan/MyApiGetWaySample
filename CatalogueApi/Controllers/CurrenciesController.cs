748082f00823e80d9ba53bf5a0930dc6cb6dbf90
Stanley Akachukwu <denmarkstan@yahoo.com>
2020-09-27 08:45:57 +0100

    first commit

---
+++ CatalogueApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogueApi.Controllers
+{
+    [ApiController]
+    [Route("api/currencies")]
+    public class CurrenciesController : ControllerBase
+    {
+        [HttpGet, Route("")]
+         public ActionResult<IEnumerable<string>> Get()
+         {
+            return new string [] {"us","bfp","naira"};
+         }
+    }
+}
